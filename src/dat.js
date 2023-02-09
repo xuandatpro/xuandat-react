5bcc438e418bd33cb7e094e836828121bd83d56c
xuandatpro <121415783+xuandatpro@users.noreply.github.com>
2023-02-09 23:42:08 +0700

    first commit

--- src/dat.js
+++ src/dat.js
@@ -1,5 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 function Dat(){
+    const[state,setState]=useState(0)
+    const handleclick=()=>{
+        setState((current)=>{
+            return current=current+1
+        })
+    }
     var isUserLoggedIn=true
-    if(isUserLoggedIn=true)
+    if(isUserLoggedIn="true"){
+        return(
+        <div class="app">
+            <h1>hello xuandat cau lenh {isUserLoggedIn}</h1>
+            <button onClick={handleclick}>click</button>
+            <h1>ban so {state}</h1>
+        </div>
+        )
+    }
 }
+export default Dat
